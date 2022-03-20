6bcf034b44152c35205026ae88727ce5a92d835a
blackBoyImran <lawjbasi@gmail.com>
2022-03-20 21:58:52 +0600

    first commit

---
+++ problem1.js
@@ -0,0 +1,34 @@
+
+
+
+///////   method 1
+let a = 5;
+let b = 10;
+
+console.log("a = "+a,"\t"+ "b = "+b)
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("a = "+a,"\t"+ "b = "+b);
+
+
+
+
+///////   method 2
+
+let p = 10;
+let q = 3;
+console.log("p = "+p,"\tq = "+q);
+
+p = p + q;
+q = p - q;
+p = p - q;
+console.log("p = "+p,"\tq= "+q);
+
+
+
+
+
+
---
+++ random-number.js
@@ -0,0 +1,31 @@
+let number = 10.6;
+// let floor = Math.floor(number);
+// let ceil = Math.ceil(number);
+// let round = Math.round(number);
+let random = Math.random(number)*10;
+let randomResult = Math.round(random);
+
+
+// console.log(floor);
+// console.log(ceil);
+// console.log(round);
+// console.log(random);
+console.log(randomResult);
+
+
+let num = 10;
+
+for(let i = 1; i<= num ;i++){
+    let random2 = Math.random(num);
+    let randomResult2 = Math.round(random2);
+    console.log(randomResult2);
+}
+
+
+
+
+
+
+
+
+
